894bf09a272dc814d820c1ec1681acd4fcfedcb9
Megan <megareceiver@gmail.com>
2018-01-06 00:34:56 +0700

    up

--- public/uniq/main.php
+++ public/uniq/main.php
@@ -18,48 +18,40 @@ $app = new \Slim\App;
 $app->get($section.'/modules/{company_id}', function(Request $request, Response $response, array $args){
   try {
     // Catch arguments
-    $company_id = $args['company_id'];
+    $company_id    = $args['company_id'];
+    // Get database object
+    $db            = new db();
+
+    // connect
+    $system_db     = $db->connection('system');
+    $membership_db = $db->connection('membership');
+    $db            = $db->connect('membership');
 
     // STEP 1 ------------------------------------------------------------------
     // Get all sub modules id on chosen plan
     $sql =
-    "SELECT pm.module_id as id_modules_sub, pm.active as active, s.name
-     FROM uniq365_membership.users u
-     JOIN uniq365_membership.payment_plan_modules pm ON u.premium_planid = pm.plan_id
-     JOIN uniq365_system.modules_sub s ON pm.module_id = s.id
+    "SELECT s.id as `id`, m.name as `module`, g.name as `group`, s.name as `name`, pm.active as active
+     FROM ".$membership_db->dbname.".users u
+     JOIN ".$membership_db->dbname.".payment_plan_modules pm ON u.premium_planid = pm.plan_id
+     JOIN ".$system_db->dbname.".modules_sub s ON pm.module_id = s.id
+     JOIN ".$system_db->dbname.".modules m ON s.id_modules = m.id
+     JOIN ".$system_db->dbname.".modules_group g ON s.id_modules_group = g.id
      WHERE u.username = '$company_id'
      ";
-
-    // Get database object
-    $db = new db();
-    // connect
-    $db = $db->connect('membership');
 
     //statement
     $statement = $db->query($sql);
     $result = $statement->fetchAll(PDO::FETCH_OBJ);
     $db = null;
 
-    // // STEP 2 ------------------------------------------------------------------
-    // // Get all sub modules
-    // $sql =
-    // "SELECT pm.module_id as id_modules_sub, pm.active as active
-    //  FROM users u
-    //  JOIN payment_plan_modules pm ON u.premium_planid = pm.plan_id
-    //  WHERE u.username = '$company_id'
-    //  ";
-    //
-    // // Get database object
-    // $db = new db();
-    // // connect
-    // $db = $db->connect('system');
-    //
-    // //statement
-    // $statement = $db->query($sql);
-    // $result = $statement->fetchAll(PDO::FETCH_OBJ);
-    // $db = null;
+    $refactor = array();
+    for($loop=0;$loop<count($result);$loop++){
+      $refactor[$result[$loop]->module][$result[$loop]->group][$loop]['id'] = $result[$loop]->id;
+      $refactor[$result[$loop]->module][$result[$loop]->group][$loop]['name'] = $result[$loop]->name;
+      $refactor[$result[$loop]->module][$result[$loop]->group][$loop]['active'] = $result[$loop]->active;
+    }
 
-    echo json_encode($result);
+    echo json_encode($refactor);
     // echo ($result[0]->id_modules_sub);
 
   } catch(PDOException $e) {
